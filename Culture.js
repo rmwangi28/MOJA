103570a11bd08743a8b1ed0d34b77f7a38710408
rmwangi28 <rachelmwangi149@gmail.com>
2025-07-16 13:09:49 -0700

    added left click effect

--- Culture.js
+++ Culture.js
@@ -1,19 +1,31 @@
 const pages = document.querySelectorAll('.page');
 let currentPage = 0;
 
-// Function to flip the book cover
-window.onload = function (){
-  pages.forEach(page => page.classsList.remove('flipped'));
+// Resets the book on load
+window.onload = function () {
+  pages.forEach(page => page.classList.remove('flipped'));
   currentPage = 0;
-}
-//Flips only on the current page
+};
+
+// Flips only on the current page
 pages.forEach((page, index) => {
-  page.addEventListener('click', () => {
-    // Flip the current page
-    page.classList.add('flipped');
-    currentPage++;
+  page.addEventListener('click', (event) => {
+    const clickX = event.clientX;
+    const pageRect = page.getBoundingClientRect();
+    const pageCenter = pageRect.left + pageRect.width / 2;
+
+    if (clickX > pageCenter && currentPage < pages.length) {
+      // if clicked on right half - flip forward
+      pages[currentPage].classList.add('flipped');
+      currentPage++;
+    } else if (clickX < pageCenter && currentPage > 0) {
+      // if clicked on the left half - flip backwards
+      currentPage--;
+      pages[currentPage].classList.remove('flipped');
+    }
   });
 });
+
 //refresh button to reset the book 
 document.getElementById('refreshBtn').addEventListener('click', () => {
   location.reload();
